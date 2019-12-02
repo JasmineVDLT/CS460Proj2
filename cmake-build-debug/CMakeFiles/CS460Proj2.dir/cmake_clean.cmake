330781622b5a11d406ad67c4bc7d4a0d52643643
Kyle de Bord <kdebord@blue.cs.sonoma.edu>
2019-12-01 22:50:45 -0800

    parses bugfree files

---
+++ cmake-build-debug/CMakeFiles/CS460Proj2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS460Proj2.dir/Project2.cpp.obj"
+  "CMakeFiles/CS460Proj2.dir/SyntacticalAnalyzer.cpp.obj"
+  "CS460Proj2.pdb"
+  "CS460Proj2.exe"
+  "CS460Proj2.exe.manifest"
+  "libCS460Proj2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS460Proj2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
